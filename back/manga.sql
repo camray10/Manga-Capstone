ed0fb02b22e46cb80b707cdd1b94129b97095aa1
Cameron Ray <rayc287@protonmail.com>
2023-04-17 17:27:46 -0400

    working on server

--- back/manga.sql
+++ back/manga.sql
@@ -6,3 +6,12 @@ CREATE DATABASE manga;
 \connect manga
 
 \i manga-schema.sql
+
+\echo 'Delete and recreate manga_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE manga_test;
+CREATE DATABASE manga_test;
+\connect manga_test
+
+\i manga-schema.sql
