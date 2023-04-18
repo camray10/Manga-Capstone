818341888989fe2e58a55f076f19f24e929046fa
Cameron Ray <rayc287@protonmail.com>
2023-04-18 17:56:44 -0400

    implemented helpers to assist with other functions

---
+++ back/helpers/sql.js
@@ -0,0 +1,38 @@
+const { BadRequestError } = require("../expressError");
+
+/**
+ * Helper for making selective update queries.
+ *
+ * The calling function can use it to make the SET clause of an SQL UPDATE
+ * statement.
+ *
+ * @param dataToUpdate {Object} {field1: newVal, field2: newVal, ...}
+ * @param jsToSql {Object} maps js-style data fields to database column names,
+ *   like { firstName: "first_name", age: "age" }
+ *
+ * @returns {Object} {sqlSetCols, dataToUpdate}
+ *
+ * @example {firstName: 'Aliya', age: 32} =>
+ *   { setCols: '"first_name"=$1, "age"=$2',
+ *     values: ['Aliya', 32] }
+ */
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // Add "registration_date" to the jsToSql object
+  const updatedJsToSql = { ...jsToSql, registrationDate: "registration_date" };
+
+  // {firstName: 'Aliya', age: 32, registrationDate: '2022-01-01'} => ['"first_name"=$1', '"age"=$2', '"registration_date"=$3']
+  const cols = keys.map((colName, idx) =>
+      `"${updatedJsToSql[colName] || colName}"=$${idx + 1}`,
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
