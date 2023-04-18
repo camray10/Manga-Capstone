5c05545b3ec9a6730a88f17fc464bd7969b4a4f2
Cameron Ray <rayc287@protonmail.com>
2023-04-18 17:56:03 -0400

    fixed backend routes and database

--- back/manga-schema.sql
+++ back/manga-schema.sql
@@ -1,6 +1,6 @@
 -- Manga table
 CREATE TABLE manga (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     synopsis TEXT NOT NULL,
     cover_image_url TEXT NOT NULL,
@@ -9,13 +9,13 @@ CREATE TABLE manga (
 
 -- Author table
 CREATE TABLE author (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
 -- Manga-Author table
 CREATE TABLE manga_author (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     manga_id INTEGER NOT NULL,
     author_id INTEGER NOT NULL,
     FOREIGN KEY (manga_id) REFERENCES manga(id),
@@ -24,16 +24,20 @@ CREATE TABLE manga_author (
 
 -- User table
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username TEXT NOT NULL,
-    email TEXT NOT NULL,
     password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    CHECK (position('@' IN email) > 1),
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
     registration_date DATE NOT NULL
 );
 
 -- Rating table
 CREATE TABLE rating (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     manga_id INTEGER NOT NULL,
     score INTEGER NOT NULL,
@@ -43,7 +47,7 @@ CREATE TABLE rating (
 
 -- Comment table
 CREATE TABLE comment (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     manga_id INTEGER NOT NULL,
     text TEXT NOT NULL,
