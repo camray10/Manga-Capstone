84af8dda6c57e4451ad984d8c1a333cb9b40cbed
Cameron Ray <rayc287@protonmail.com>
2023-05-04 03:23:19 -0400

    user auth

--- back/models/user.js
+++ back/models/user.js
@@ -145,13 +145,7 @@ static async get(username) {
   const user = userRes.rows[0];
 
   if (!user) throw new NotFoundError(`No user: ${username}`);
-
-  const userApplicationsRes = await db.query(
-    `SELECT a.job_id
-     FROM applications AS a
-     WHERE a.username = $1`, [username]);
-
-  user.applications = userApplicationsRes.rows.map(a => a.job_id);
+  
   return user;
 }
 
