9bbb98c047b674c2854681b48641643077cf525c
Cameron Ray <rayc287@protonmail.com>
2023-05-04 03:20:32 -0400

    fixed token issues

--- back/helpers/tokens.js
+++ back/helpers/tokens.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 
 /** return signed JWT from user data. */
-
 function createToken(user) {
   console.assert(user.isAdmin !== undefined,
       "createToken passed user without isAdmin property");
@@ -12,7 +11,12 @@ function createToken(user) {
     isAdmin: user.isAdmin || false,
   };
 
-  return jwt.sign(payload, SECRET_KEY);
+  const signedToken = jwt.sign(payload, SECRET_KEY);
+  console.log('Payload:', payload);
+  console.log('Signed Token:', signedToken);
+  
+  return signedToken;
 }
+
 
 module.exports = { createToken };
