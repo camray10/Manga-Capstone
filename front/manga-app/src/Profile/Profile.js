ad300ffc3ced81a1bd37b0a8a3180e1a8620f6e3
Cameron Ray <rayc287@protonmail.com>
2023-05-04 03:12:56 -0400

    updated profile

---
+++ front/manga-app/src/Profile/Profile.js
@@ -0,0 +1,129 @@
+import React, { useState, useContext } from "react";
+import MangaApi from "../API/api";
+import UserContext from "../Hooks/UserContext";
+import "../Styles/Profile.css";
+
+/** Profile editing form.
+ *
+ * Displays profile form and handles changes to local form state.
+ * Submitting the form calls the API to save, and triggers user reloading
+ * throughout the site.
+ */
+
+function ProfileForm() {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    firstName: currentUser.firstName,
+    lastName: currentUser.lastName,
+    email: currentUser.email,
+    username: currentUser.username,
+    password: "",
+  });
+  const [formErrors, setFormErrors] = useState([]);
+
+  console.debug(
+      "ProfileForm",
+      "currentUser=", currentUser,
+      "formData=", formData,
+      "formErrors=", formErrors,
+  );
+
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+
+    let profileData = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      password: formData.password,
+    };
+
+    let username = formData.username;
+    let updatedUser;
+
+    try {
+      updatedUser = await MangaApi.saveProfile(username, profileData);
+    } catch (errors) {
+      setFormErrors(errors);
+      return;
+    }
+
+    setFormData(f => ({ ...f, password: "" }));
+    setFormErrors([]);
+
+    // trigger reloading of user information throughout the site
+    setCurrentUser(updatedUser);
+  }
+
+  /** Handle form data changing */
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData(f => ({
+      ...f,
+      [name]: value,
+    }));
+    setFormErrors([]);
+  }
+
+  return (
+      <div className="profile">
+        <h3>Profile</h3>
+        <div className="card">
+          <div className="card-body">
+            <form>
+              <div className="form-group-username">
+                <label>Username</label>
+                <p className="form-control">{formData.username}</p>
+              </div>
+              <div className="form-group">
+                <label>First Name</label>
+                <input
+                    name="firstName"
+                    className="form-control"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label>Last Name</label>
+                <input
+                    name="lastName"
+                    className="form-control"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label>Email</label>
+                <input
+                    name="email"
+                    className="form-control"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label>Confirm password to make changes:</label>
+                <input
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+              </div>
+
+              <button
+                  className="btn-profile"
+                  onClick={handleSubmit}
+              >
+                Save Changes
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default ProfileForm;
